a70bf8b33ffc6e18665836ee81b34a79dc7bd65d
mdraselmiah95 <russelroy15@gmail.com>
2023-08-19 16:39:23 +0600

    Learning Cypress

---
+++ cypress-course/app/layout.js
@@ -0,0 +1,21 @@
+import NavBar from "./components/NavBar";
+import "./globals.css";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Cypress-Testing",
+  description: "Learning Cypress testing",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <NavBar />
+        {children}
+      </body>
+    </html>
+  );
+}
